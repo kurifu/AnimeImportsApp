cd12b1fc63b932a8198d2e4fadcd81f9b110574a
Cliff Chen <kurifuC4@gmail.com>
2011-10-15 20:20:35 -0700

    Working version with GoogleAPI, only pulls events from calendar you own though; includes custom ArrayAdapter for better ListItems

--- gen/net/animeimports/android/R.java
+++ gen/net/animeimports/android/R.java
@@ -13,11 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int bottomtext=0x7f050004;
+        public static final int icon=0x7f050002;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050001;
+        public static final int toptext=0x7f050003;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int event=0x7f030000;
+        public static final int event_browser=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int main_menu_option=0x7f030003;
+        public static final int row=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int back=0x7f040006;
+        public static final int delete=0x7f040004;
+        public static final int main_header=0x7f040005;
+        public static final int new_calendar=0x7f040002;
+        public static final int switch_account=0x7f040001;
+        public static final int update_title=0x7f040003;
     }
 }
---
+++ src/net/animeimports/android/AIEventEntry.java
@@ -0,0 +1,50 @@
+package net.animeimports.android;
+
+public class AIEventEntry {
+	private String name;
+	private String date;
+	private String time;
+	
+	public String toString() {
+		//System.out.println("CHECK: date: " + date + ", time: " + time + ", name: " + name);
+		return date + ", " + time + "\t" + name;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the date
+	 */
+	public String getDate() {
+		return date;
+	}
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(String date) {
+		this.date = date;
+	}
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
