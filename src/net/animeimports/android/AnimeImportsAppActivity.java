448c1a9c0776a6b24d9653e9a30fe6b165222498
Cliff Chen <kurifuC4@gmail.com>
2012-01-21 22:50:24 -0800

    Refactoring: moved calendar related code into a separate class, cleaning up various other sections

--- src/net/animeimports/android/AnimeImportsAppActivity.java
+++ src/net/animeimports/android/AnimeImportsAppActivity.java
@@ -20,17 +20,10 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
 import com.google.api.services.calendar.CalendarClient;
-import com.google.api.services.calendar.CalendarRequestInitializer;
 import com.google.api.services.calendar.CalendarUrl;
 import com.google.api.services.calendar.model.EventEntry;
 import com.google.api.services.calendar.model.EventFeed;
-import com.google.api.client.extensions.android2.AndroidHttp;
-import com.google.api.client.googleapis.GoogleHeaders;
-import com.google.api.client.googleapis.extensions.android2.auth.GoogleAccountManager;
-import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.util.DateTime;
 import com.google.common.collect.Lists;
 
@@ -49,7 +42,8 @@ import android.widget.Toast;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import net.animeimports.calendar.AICalendarManager;
+import net.animeimports.calendar.AICalendarManager.CalendarAndroidRequestInitializer;
 import net.animeimports.calendar.AIEventAdapter;
 import net.animeimports.calendar.AIEventEntry;
 import net.animeimports.calendar.AIEventEntry.EVENT_TYPE;
@@ -62,25 +56,12 @@ import net.animeimports.league.XmlParser;
 
 public class AnimeImportsAppActivity extends ListActivity {
 	
-	// GoogleCalendar
-	private static final String TAG = "CalendarSample";
-	final HttpTransport transport = AndroidHttp.newCompatibleTransport();
-	static final String PREF = TAG;
-	static final String PREF_ACCOUNT_NAME = "accountName";
-	static final String PREF_AUTH_TOKEN = "authToken";
-	static final String PREF_GSESSIONID = "gsessionid";
-	GoogleAccountManager accountManager;
-	HttpRequestFactory requestFactory;
-	SharedPreferences settings;
-	String accountName;
-	String authToken;
 	CalendarClient client;
 	CalendarAndroidRequestInitializer requestInitializer;
 	
 	// Events / Lists
 	private List<String> storeInfo = Lists.newArrayList();
 	private AIEventAdapter aiEventAdapter;
-	private static final int DAYS_IN_FUTURE = 14;
 	
 	private int currMenu = 0;
 	private final int NEWS = 1;
@@ -109,43 +90,7 @@ public class AnimeImportsAppActivity extends ListActivity {
 	TextView tvSessionHeader = null;
 	TextView tvLifetimeHeader = null;
 	
-	/**
-	 * Old code from GoogleCalendar Api examples, not entirely sure this is being used... 
-	 * TODO: flag for delete
-	 */
- 	public class CalendarAndroidRequestInitializer extends CalendarRequestInitializer {
-		String authToken;
-		
-		public CalendarAndroidRequestInitializer() {
-			super(transport);
-			authToken = settings.getString(PREF_AUTH_TOKEN, null);
-			setGsessionid(settings.getString(PREF_GSESSIONID, null));
-		}
-		
-		public void intercept(HttpRequest request) throws IOException {
-			super.intercept(request);
-			request.getHeaders().setAuthorization(GoogleHeaders.getGoogleLoginValue(authToken));
-		}
-
-		public boolean handleResponse(HttpRequest request, HttpResponse response, boolean retrySupported) throws IOException {
-			switch(response.getStatusCode()) {
-				case 302:
-					super.handleResponse(request, response, retrySupported);
-					SharedPreferences.Editor editor = settings.edit();
-					editor.putString(PREF_GSESSIONID, getGsessionid());
-					editor.commit();
-					return true;
-				case 401:
-					accountManager.invalidateAuthToken(authToken);
-					authToken = null;
-					SharedPreferences.Editor editor2 = settings.edit();
-					editor2.remove(PREF_AUTH_TOKEN);
-					editor2.commit();
-					return false;
-			}
-			return false;
-		}
-	}
+	AICalendarManager cManager = null;
 	
     /**
      *  Called when the activity is first created. 
@@ -162,9 +107,8 @@ public class AnimeImportsAppActivity extends ListActivity {
      */
     public void initializeApp() {
     	events = new ArrayList<AIEventEntry>();
-		accountManager = new GoogleAccountManager(this);
-	    settings = this.getSharedPreferences(PREF, 0);
-	    requestInitializer = new CalendarAndroidRequestInitializer();
+	    cManager = AICalendarManager.getInstance(getApplicationContext());
+	    requestInitializer = cManager.getCalReqInitializer();
 	    client = new CalendarClient(requestInitializer.createRequestFactory());
 	    
     	imgInfo = (ImageView) findViewById(R.id.imgInfo);
@@ -322,30 +266,6 @@ public class AnimeImportsAppActivity extends ListActivity {
     	}
     }
     
-    private Date getStartDate() {
-    	Calendar now = Calendar.getInstance();
-    	Date startDate = new Date();
-		startDate.setYear(now.get(Calendar.YEAR)-1900);
-		startDate.setMonth(now.get(Calendar.MONTH));
-		startDate.setDate(now.get(Calendar.DATE));
-		startDate.setHours(now.get(Calendar.HOUR));
-		startDate.setMinutes(now.get(Calendar.MINUTE));
-		startDate.setSeconds(now.get(Calendar.SECOND));
-		return startDate;
-    }
-    
-    private Date getEndDate() {
-    	Calendar now = Calendar.getInstance();
-		Date endDate = new Date();
-		endDate.setYear(now.get(Calendar.YEAR)-1900);
-		endDate.setMonth(now.get(Calendar.MONTH));
-		endDate.setDate(now.get(Calendar.DATE) + DAYS_IN_FUTURE);
-		endDate.setHours(now.get(Calendar.HOUR));
-		endDate.setMinutes(now.get(Calendar.MINUTE));
-		endDate.setSeconds(now.get(Calendar.SECOND));
-		return endDate;
-    }
-    
     /**
      * Populate the array adapter
      */
@@ -373,7 +293,7 @@ public class AnimeImportsAppActivity extends ListActivity {
     void getLeague() {
     	if(leagueStats == null)
     		mProgressDialog = ProgressDialog.show(this, "Please wait...", "Retrieving leaderboard...");
-    	Thread thread = new Thread(null, leagueFetchThread, "MagentoBackground");
+    	Thread thread = new Thread(null, leagueFetchThread, "leagueFetchThread");
     	thread.start();
     }
     
@@ -403,15 +323,11 @@ public class AnimeImportsAppActivity extends ListActivity {
 	        	catch (UnknownHostException e) {
 	        		runOnUiThread(recoverThread);
 	        		loadNews();
-		    		//runOnUiThread(loadMainMenuThread);
 		    		return;
 	        	}
 	        	catch (IOException e) {
 	    			e.printStackTrace();
 	    		}
-	        	/*catch(HttpException e) {
-	        		
-	        	}*/
         	}
         	
     		if(mProgressDialog != null)
@@ -449,7 +365,7 @@ public class AnimeImportsAppActivity extends ListActivity {
      */
     void getEvents() {
     	mProgressDialog = ProgressDialog.show(AnimeImportsAppActivity.this, "Please wait...", "Retrieving data...", true);
-		Thread thread = new Thread(null, eventFetchThread, "MagentoBackground");
+		Thread thread = new Thread(null, eventFetchThread, "eventFetchThread");
 		thread.start();
     }
     
@@ -464,8 +380,8 @@ public class AnimeImportsAppActivity extends ListActivity {
 	    	
 	    	try {
 	    		CustomCalendarURL customUrl = CustomCalendarURL.getUrl();
-	    		customUrl.startMin = new DateTime(getStartDate());
-	    		customUrl.startMax = new DateTime(getEndDate());
+	    		customUrl.startMin = new DateTime(cManager.getStartDate());
+	    		customUrl.startMax = new DateTime(cManager.getEndDate());
 	        	CalendarUrl url = new CalendarUrl(customUrl.build());
 	    		EventFeed feed = client.eventFeed().list().execute(url);
 
@@ -507,6 +423,7 @@ public class AnimeImportsAppActivity extends ListActivity {
 	    	catch(SocketTimeoutException e) {
 	    		runOnUiThread(recoverThread);
 	    		loadNews();
+	    		return;
 	    	}
 	    	catch(IOException e) {
 	    		System.out.println("IOException yo");
